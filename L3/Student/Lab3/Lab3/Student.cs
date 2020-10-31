de20ab073036fc937b449ca0a3fb59767b10a753
jobairh <jobairh999@gmail.com>
2020-10-31 10:16:13 +0600

    L3 done

--- L3/Student/Lab3/Lab3/Student.cs
+++ L3/Student/Lab3/Lab3/Student.cs
@@ -8,5 +8,38 @@ namespace Lab3
 {
     class Student
     {
+        private string name;
+        private string id;
+        private string department;
+        private float cgpa;
+
+        public string Id
+        {
+            set { id = value; }
+            get { return id; }
+        }
+        public string Name
+        {
+            set { name = value; }
+            get { return name; }
+        }
+        public string Department
+        {
+            set { department = value; }
+            get { return department; }
+        }
+        public float Cgpa
+        {
+            set { cgpa = value; }
+            get { return cgpa; }
+        }
+        public Student() { }
+        public Student(string id,string name,string department,float cgpa)
+        {
+            this.id = id;
+            this.name = name;
+            this.department = department;
+            this.cgpa = cgpa;
+        }
     }
 }
